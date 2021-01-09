c381c2e7a380f5c92cee98c0108f06b601bf196c
KAAAsS <admin@kaaass.net>
2021-01-09 20:46:29 +0800

    recovery模块审计

--- src/main/java/net/kaaass/rumbase/recovery/RecoveryManager.java
+++ src/main/java/net/kaaass/rumbase/recovery/RecoveryManager.java
@@ -2,13 +2,17 @@ package net.kaaass.rumbase.recovery;
 
 import net.kaaass.rumbase.recovery.mock.MockRecoveryStorage;
 
+/**
+ * TODO 文档
+ */
 public class RecoveryManager {
     /**
      * 对某个数据库文件进行恢复，并且返回对应的日志管理器
+     *
      * @param fileName 文件名
      * @return 数据库日志管理器
      */
-    public static IRecoveryStorage recovery(String fileName){
+    public static IRecoveryStorage recovery(String fileName) {
         // TODO:对数据进行恢复
         return new MockRecoveryStorage();
     }
