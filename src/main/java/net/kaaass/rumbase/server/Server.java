43aeb63df6049da1f51faab0b78e16703ebd41ac
KAAAsS <admin@kaaass.net>
2021-01-17 00:09:30 +0800

    创建数据文件夹

--- src/main/java/net/kaaass/rumbase/server/Server.java
+++ src/main/java/net/kaaass/rumbase/server/Server.java
@@ -9,6 +9,7 @@ import net.kaaass.rumbase.table.TableManager;
 import net.kaaass.rumbase.transaction.TransactionManager;
 import net.kaaass.rumbase.transaction.TransactionManagerImpl;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.SocketTimeoutException;
@@ -44,6 +45,11 @@ public class Server {
      * 进行服务器开始前的准备工作
      */
     public void prepare() {
+        // 准备文件夹
+        var tableFolder = new File("data/table/a");
+        assert tableFolder.exists() || tableFolder.mkdirs();
+        var indexFolder = new File("data/index/a");
+        assert indexFolder.exists() || indexFolder.mkdirs();
         // 初始化事务管理器
         log.info("初始化事务管理器...");
         try {
@@ -69,7 +75,7 @@ public class Server {
      */
     public void run() {
         // 监听端口
-        try (ServerSocket server = new ServerSocket(8994)) {
+        try (ServerSocket server = new ServerSocket(8888)) {
             log.info("开始监听 {}", server.getLocalSocketAddress());
 
             server.setSoTimeout(1000);
